7743a6d32e6cf6269115c4d0e1d4aea1c75042fb
AndreeaHP <andreeahulpoi@gmail.com>
2023-08-31 09:00:18 +0300

    FizzBuzz exercise

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# TemaFIZZBUZZ
-# Se citeste un numar intreg de la tastatura
+# # Tema 1 - FIZZBUZZ
+# # Se citeste un numar intreg de la tastatura
+# #
+# # Daca numarul este divizibil cu:
+# # 3 - se afiseaza Fizz
+# # 5 - se afiseaza Buzz
+# # 3 si 5 - se afiseaza FizzBuzz
+# # In caz contrar se afiseaza numarul citit
 #
-# Daca numarul este divizibil cu:
-# 3 - se afiseaza Fizz
-# 5 - se afiseaza Buzz
-# 3 si 5 - se afiseaza FizzBuzz
-# In caz contrar se afiseaza numarul citit
-
-valoare_primita = int(input("Numarul intreg este = "))
-
-if valoare_primita % 3 == 0 and valoare_primita % 5 == 0:
-    print("FizzBuzz")
-elif valoare_primita % 3 == 0:
-    print("Fizz")
-elif valoare_primita % 5 == 0:
-    print("Buzz")
-else:
-    print(valoare_primita)
+# valoare_primita = int(input("Numarul intreg este = "))
+#
+# if valoare_primita % 3 == 0 and valoare_primita % 5 == 0:
+#     print("FizzBuzz")
+# elif valoare_primita % 3 == 0:
+#     print("Fizz")
+# elif valoare_primita % 5 == 0:
+#     print("Buzz")
+# else:
+#     print(valoare_primita)
