845aefec2a8faf75564367a0e7b19dfa0f502a97
AndreeaHP <110348170+AndreeaHP@users.noreply.github.com>
2023-09-16 18:54:26 +0300

    CNP Validator

--- CNP_validator.py
+++ CNP_validator.py
@@ -10,9 +10,9 @@ def validate_cnp(cnp):
 #validam daca cifrele pt sex sunt valide
 validation = validate_cnp(cnp)
 if validation:
-    print('CNP is valid!')
+    print('lenght/char CNP is valid!')
 else:
-    print('CNP is not valid!')
+    print('lenght/char CNP is not valid!')
 
 def validate_sex_digit(cnp):
     if cnp[0] == '0':
@@ -21,9 +21,9 @@ def validate_sex_digit(cnp):
         return True
 validate_sex = validate_sex_digit(cnp)
 if validate_sex:
-    print('CNP is valid!')
+    print('sex CNP is valid!')
 else:
-    print('CNP is not valid!')
+    print('sex CNP is not valid!')
 
 #validam daca cifrele pt anul nasterii sunt valide
 def validate_year_digit(cnp):
@@ -33,9 +33,9 @@ def validate_year_digit(cnp):
         return True
 validate_year = validate_year_digit(cnp)
 if validate_year:
-    print('CNP is valid!')
+    print('year CNP is valid!')
 else:
-    print('CNP is not valid!')
+    print('year CNP is not valid!')
 
 # validam daca cifrele pt luna nasterii sunt valide
 def validate_month_digit(cnp):
@@ -45,9 +45,9 @@ def validate_month_digit(cnp):
         return True
 validate_month = validate_month_digit(cnp)
 if validate_month:
-    print('CNP is valid!')
+    print('month CNP is valid!')
 else:
-    print('CNP is not valid!')
+    print('month CNP is not valid!')
 
 # validam daca cifrele pt ziua nasterii sunt valide
 def validate_day_digit(cnp):
@@ -60,27 +60,38 @@ def validate_day_digit(cnp):
         return True
 validate_day = validate_day_digit(cnp)
 if validate_day:
-    print('CNP is valid!')
+    print('day CNP is valid!')
 else:
-    print('CNP is not valid!')
+    print('day CNP is not valid!')
 
 # validam daca cifrele pt judet sunt valide
 def validate_county_digit(cnp):
-    if cnp[7:9] < 1 or cnp[7:9] > 46 or != ['51', '52']:   #DE CONTINUAT
+    if cnp[7:9] < 1 or cnp[7:9] > 46 or cnp != ['51', '52']:   #DE CONTINUAT
         return False
     else:
         return True
 validate_day = validate_day_digit(cnp)
 if validate_day:
-    print('CNP is valid!')
+    print('county CNP is valid!')
 else:
-    print('CNP is not valid!')
+    print('county CNP is not valid!')
 
+# validam daca cifrele NNN sunt valide
 
+def validate_NNN_digit(cnp):
+    if int(cnp[9:12]) < 1 or int(cnp[9:12]) > 999:
+        return False
+    else:
+        return True
+validate_NNN = validate_NNN_digit(cnp)
+if validate_NNN:
+    print('NNN CNP is valid!')
+else:
+    print('NNN CNP is not valid!')
 #if validare_sex & validare_an & validare_luna & celelalte validari (zi, judet, cod): return False  = pentru a pune toate validari intr-un If
 
 
-# 2850302270061
+
 
 
 
